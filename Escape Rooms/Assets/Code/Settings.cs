7b37911709e66f94cf24b5154a0ca326adc2af3d
Mustafa El Sharif <mustafaelsharif01@gmail.com>
2016-07-02 05:29:50 -0400

    Setting Stuff

---
+++ Escape Rooms/Assets/Code/Settings.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void setVolume()
+    {
+        
+        AudioListener.volume = GameObject.Find("Canvas/Volume Bar").GetComponent<Slider>().value;
+
+    }
+
+    public void setSensitivity()
+    {
+        GameManager.instance.sensitivity = GameObject.Find("Canvas/SensitivityBar").GetComponent<Slider>().value;
+    }
+
+    public void backToMainMenu()
+    {
+        GameManager.instance.LoadMenu();
+    }
+
+    public void loadSettingsScene()
+    {
+        GameManager.instance.LoadSettings();
+    }
+}
